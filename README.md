185c7d40c3373deb5150087e59a2f2417a5fda48
Jully <jullyksilva@hotmail.com>
2019-05-21 01:48:39 -0300

    Adicionado o arquivo principal (main)

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 # Lab-II_TP_IBGE
 Trabalho Prático - Laboratório de Computação II - Manipulação de Dados para Censo IBGE
+
+# Cadastro de usuário
+- ID                
+- IDADE
+- NOME
+- SEXO
+- RAÇA
+- LOCAL DE MORADIA
+- ESTADO CIVIL
+
+* Na formatação, deve separar as informações usando " ; " ou  " | " 
+
+# Pesquisa de Dados
+- Procurar o registro de uma pessoa cadastrada, usando o ID
+- Exibir os dados da pessoa pesquisada
+
+# Inserção / Exclusão de dados
+- Verificar se o usuário já está cadastrado
+- Exibir mensagem caso já tenha
+
+# Alteração de dados
+- Fazendo a busca pelo ID
+- Atualizar os dados do usuário
+
+# Relatório (Porcentagem e valor absoluto)
+- População total
+- Sexo (F / M)
+- Estado civil (solteiro / casado / divorciado / viúvo)
+- Raça (preta / branca / amarela / indígena)
+- Moradia (rural / urbano)
+- Faixa etária (0-12 / 13-19 / 20-25 / 26-30 / ...)
+- MÉDIA DE IDADE: através de Sexo e tipo de Moradia
+- MÉDIA DE IDADE e DESVIO PADRÃO de solteiro e casados
---
+++ SistemaIBGE-main.java
@@ -0,0 +1,59 @@
+import java.util.Scanner;
+
+public class SistemaIBGE {
+
+	public static void main(String[] args) {
+		Scanner entrada = new Scanner(System.in);
+		int op, ID;
+		do {
+			System.out.print("\t\t\tMENU DE OPÇÕES\n\n");
+			System.out.print("\t\t1- Pesquisar dado de uma pessoa\n\t\t2- Excluir dado de uma pessoa\n\t\t3 - Alterar dados de uma pesoa\n");
+			System.out.print("\t\t4- Inserir dados de uma pessoa\n\t\t5- Gerar relatório populacional\n\t\t6- Gerar relatório estatístico\n");
+			System.out.print("\t\t7- Sair do programa\n\n\n\tEscolha: ");
+			op = entrada.nextInt();
+			switch(op){
+				case 1:
+					//Limpa a tela no windows, no linux e no MacOS, é claro que fica a discussão de nós decidirmos
+			        /*if (System.getProperty("os.name").contains("Windows"))
+			            new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+			        else
+			            Runtime.getRuntime().exec("clear");*/
+					System.out.print("Insira o ID da pessoa a ser procurada: ");
+					ID = entrada.nextInt();
+					System.out.print("DADOS:\n" + PesquisarPessoa(ID));
+				break;
+				case 2:
+					System.out.print("Para a exlusão, digite o ID da pessoa a ser procurada: ");
+					ID = entrada.nextInt();
+					RemoverPessoa(ID);
+				break;
+				case 3:
+					System.out.print("Alteração de dados, digite o ID da pessoa: ");
+					ID = entrada.nextInt();
+					AlterarPessoa(ID);
+				break;
+				case 4:
+					System.out.print("Adicionar dados, digite o ID: ");
+					ID = entrada.nextInt();
+					AdicionarPessoa(ID);
+				break;
+				case 5:
+					System.out.print("\t\tRELATÓRIO POPULACIONAL\n");
+					RelatorioGeral(ID);
+				break;
+				case 6:
+					System.out.print("\t\tRELATÓRIO ESTATÍSTICO\n");
+					RelatorioEstatistico();
+				break;
+				case 7:
+					System.out.print("\t\t\n\nObrigado pela atenção !\n\n");
+				break;
+				default:
+					System.out.print("\t\t*** Opção digitada está incorreta! ***\n\n: ");
+				break;
+			}
+		}while(op != 7);
+
+	}
+
+}
