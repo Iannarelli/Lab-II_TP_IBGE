a3f3a8a4c11c5ca424bd54f8e613d1fe7dea9ae7
Jully <jullyksilva@hotmail.com>
2019-06-06 20:33:43 -0300

    Classes de tipo Pessoa e relatorio

---
+++ GerarRelatorio.java
@@ -0,0 +1,36 @@
+
+public class GerarRelatorio {
+	
+	public void populacaoToral(){
+		
+	}
+	
+	public void taxaSexo(){
+		
+	}
+	
+	public void EstadoCivil(){
+		
+	
+	}
+	
+	public void RacaPopu(){
+		
+	}
+	
+	public void Moradia(){
+		
+	}
+	
+	public void faixaEtaria(){
+		
+	}
+	
+	public void mediaIdade(){
+		
+	}
+	
+	public void MediaIdesvioP() {
+		
+	}
+}
